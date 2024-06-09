ea942a320ec6f346bbeb064c5476405e4ae2e3f2
shimishimizu <h200l700@gmail.com>
2024-06-09 17:11:57 +0900

    DBリアルタイム検知処理を追加

--- hooks/useTodoList.ts
+++ hooks/useTodoList.ts
@@ -1,12 +1,14 @@
 import { Todo } from "@/utils/interface";
+import { supabase } from "@/utils/supabase";
 import { deleteTodo, getAllTodos, insertTodo } from "@/utils/supabaseFunction";
 import { useEffect, useState } from "react";
 
 export const useTodoList = () => {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [todoList, setTodoList] = useState<any[]>([]);
   const [title, setTitle] = useState("");
 
   useEffect(() => {
+    // Todo全件取得
     const getTodos = async () => {
       const todos = await getAllTodos();
       if (todos) {
@@ -14,24 +16,57 @@ export const useTodoList = () => {
       }
     };
     getTodos();
+
+    fetchRealTimeData();
   }, []);
 
+  // リアルタイム検知
+  const fetchRealTimeData = () => {
+    supabase
+      .channel("todo")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "todo" },
+        (payload) => {
+          const newTodo = [payload.new];
+          setTodoList((todoList) => [...todoList, ...newTodo]);
+        }
+      )
+      .on(
+        "postgres_changes",
+        { event: "DELETE", schema: "public", table: "todo" },
+        (payload) => {
+          setTodoList((todoList) =>
+            todoList.filter((todo) => todo.id !== payload.old.id)
+          );
+        }
+      )
+      .subscribe();
+
+    // リスナーの解除
+    return () => {
+      supabase.channel("todo").unsubscribe();
+    };
+  };
+
+  // Todo追加
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await insertTodo(title);
-    const todos = await getAllTodos();
-    if (todos) {
-      setTodoList(todos);
-    }
+    // const todos = await getAllTodos();
+    // if (todos) {
+    // setTodoList(todos);
+    // }
     setTitle("");
   };
 
+  // Todo削除
   const handleDelete = async (id: number) => {
     await deleteTodo(id);
-    const todos = await getAllTodos();
-    if (todos) {
-      setTodoList(todos);
-    }
+    // const todos = await getAllTodos();
+    // if (todos) {
+    //   setTodoList(todos);
+    // }
   };
 
   return { todoList, title, setTitle, handleSubmit, handleDelete };
